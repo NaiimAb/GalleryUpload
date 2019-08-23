5072340323fc0e90fc8010215da0317263cbd7ba
Naiim <fearnaiim@gmail.com>
2019-08-23 02:56:26 +0100

    Adding Simple layout to the app with ViewModels of whole Gallery and each Gallery item
    Signed-off-by: Naiim <fearnaiim@gmail.com>

---
+++ app/src/main/java/com/spark/test/galleryupload/viewmodel/GalleryItemViewModel.java
@@ -0,0 +1,7 @@
+package com.spark.test.galleryupload.viewmodel;
+
+import androidx.databinding.BaseObservable;
+
+public class GalleryItemViewModel extends BaseObservable {
+
+}
---
+++ app/src/main/java/com/spark/test/galleryupload/viewmodel/GalleryViewModel.java
@@ -0,0 +1,7 @@
+package com.spark.test.galleryupload.viewmodel;
+
+import java.util.Observable;
+
+public class GalleryViewModel extends Observable {
+
+}
